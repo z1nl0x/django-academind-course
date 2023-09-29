4faa00607c27ca2e7f7b1eab457b925bec412277
Paulo Kreft <paulo.kreft@gmail.com>
2023-09-29 11:29:27 -0300

    initializing Section 14

--- section_09/my_site/blog/views.py
+++ section_09/my_site/blog/views.py
@@ -1,6 +1,8 @@
+from typing import Any
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # all_posts = [
 #     {
@@ -76,6 +78,33 @@ all_posts = [
 #     return post['date']
 
 # Create your views here.
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+    
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
 
 def starting_page(request):
     latest_posts = Post.objects.all().order_by("-date")[:3]
